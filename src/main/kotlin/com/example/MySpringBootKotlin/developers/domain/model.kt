2ecdd310c4da5f03994e7aee86e3412c05e78d15
lizeyang1995 <424011527@qq.com>
2020-12-28 10:59:46 +0800

    [lizeyang] feat: create model

--- src/main/kotlin/com/example/MySpringBootKotlin/developers/domain/model.kt
+++ src/main/kotlin/com/example/MySpringBootKotlin/developers/domain/model.kt
@@ -1,2 +1,27 @@
 package com.example.MySpringBootKotlin.developers.domain
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.lang.RuntimeException
+import java.util.*
+
+typealias PasswordEncoder = (String) -> String
+
+data class Developer(
+        val username: String,
+        val email: String,
+        val password: String,
+        val id: UUID? = null
+)
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+object StorageError : RuntimeException("Internal storage error")
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+object NotFound : RuntimeException("Developer not found")
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+object NotKarumier : RuntimeException("Developer isn't karumier")
+
+@ResponseStatus(HttpStatus.CONFLICT)
+object AlreadyRegistered : RuntimeException("Developer already registered")
